1d1aba7e9bb7f01fd9ae1e2358d75bbd67cb5f39
tfuquirk <timothyfquirk@gmail.com>
2019-02-21 10:00:30 -0500

    selecting bar changes content in display div

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from "react";
 
-const MenuBar = (props) => {
-
+const MenuBar = props => {
   /*
 
-  The 'a' tags below are the menu items. Think about the way a menu 
+  The 'a' tags below are the menu items. Think about the way a menu
   should work. When you click a menu item, the button typically becomes
   'active' to indicate that it is currently selected. How could we achieve
   this programatically? What other behavior do we expect when we click
@@ -15,24 +14,23 @@ const MenuBar = (props) => {
 
   return (
     <div className="ui four item menu">
-      <a className="item active" id="profile">
-        <i className="user large icon" id="profile"/>
+      <a className="item active" id="profile" onClick={props.setPage}>
+        <i className="user large icon" id="profile" />
       </a>
 
-      <a className="item" id="photo">
-        <i className="photo large icon" id="photo"/>
+      <a className="item" id="photo" onClick={props.setPage}>
+        <i className="photo large icon" id="photo" />
       </a>
 
-      <a className="item" id="cocktail">
-        <i className="cocktail large icon" id="cocktail"/>
+      <a className="item" id="cocktail" onClick={props.setPage}>
+        <i className="cocktail large icon" id="cocktail" />
       </a>
 
-      <a className="item" id="pokemon"> 
-        <i className=" themeisle large icon" id="pokemon"/>
+      <a className="item" id="pokemon" onClick={props.setPage}>
+        <i className=" themeisle large icon" id="pokemon" />
       </a>
     </div>
-  )
+  );
+};
 
-}
-
-export default MenuBar
+export default MenuBar;
